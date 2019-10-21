bb97a38a095e5fe5508c52785a12daa98bace7bf
Aiman Almureish <aiman.almureish@gmail.com>
2019-10-21 17:25:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ prepend.unshift("Odie");
 const removeLast = ["Milo", "Otis", "Garfield"];
 removeLast.pop("Garfield");
 
-const removefirst = [];
+const removefirst = ["Milo", "Otis", "Garfield"];
 removefirst.shift();
 
