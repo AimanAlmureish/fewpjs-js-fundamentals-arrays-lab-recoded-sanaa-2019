79ff3c42e579ac9f59caf2009f0e93229740a592
Aiman Almureish <aiman.almureish@gmail.com>
2019-10-21 17:11:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-const append = ["Milo", "Otis", "Garfield"];
+const append = [];
 append.push("Odie");
 // const prepend = ["Milo", "Otis", "Garfield"];
 // prepend.shift("Odie");
