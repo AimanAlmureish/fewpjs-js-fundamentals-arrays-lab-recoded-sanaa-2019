534e89a3a0a5e7c0423a57a7a9ce36251c1f7c55
Aiman Almureish <aiman.almureish@gmail.com>
2019-10-21 17:12:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your solution here!
 const append = ["Milo", "Otis", "Garfield"];
 append.push("Odie");
-// const prepend = ["Milo", "Otis", "Garfield"];
-// prepend.shift("Odie");
+const prepend = ["Milo", "Otis", "Garfield"];
+prepend.shift("Odie");
 
 // const removeLast = ["Milo", "Otis", "Garfield"];
 // removeLast.push("Garfield");
